f2d14bd89d27707c267a13fc83a9778eca1e4909
Jorge Salcedo <jorgeto86@gmail.com>
2016-12-20 23:57:43 +0100

    [WIP] Adding router

--- index.ios.js
+++ index.ios.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
+import { Router, Scene } from 'react-native-router-flux';
 import { AppRegistry } from 'react-native';
-import Login from './scenes/login'
 
-export default class popolution extends Component {
+import Login from './scenes/login'
+import Search from './scenes/Search';
+import PageTwo from './scenes/PageTwo';
+
+export default class Popolution extends Component {
   componentDidMount() {
     console.info('Cargué la app para Android');
   }
 
   render() {
-    return <Login />
+    return (
+      <Router>
+        <Scene key="root">
+          <Scene key="login" component={Login} title="Login" initial={true} />
+          <Scene key="search" component={Search} title="Search" />
+          <Scene key="pageTwo" component={PageTwo} title="PageTwo" />
+        </Scene>
+      </Router>
+    )
   }
 }
 
-AppRegistry.registerComponent('popolution', () => popolution);
+AppRegistry.registerComponent('popolution', () => Popolution);
